5a133ef83ee2b8fc601a08beda42b09c2d34a0da
Brad Quinton <60130975+quintobl@users.noreply.github.com>
2021-11-03 00:37:39 -0400

    Add files via upload

---
+++ PolynomialRootFinder/PolynomialRootFinder/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿
+namespace PolynomialRootFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtACoefficient = new System.Windows.Forms.TextBox();
+            this.txtBCoefficient = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtCCoefficient = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.txtFirstRoot = new System.Windows.Forms.TextBox();
+            this.txtSecondRoot = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtACoefficient
+            // 
+            this.txtACoefficient.Location = new System.Drawing.Point(73, 114);
+            this.txtACoefficient.Name = "txtACoefficient";
+            this.txtACoefficient.Size = new System.Drawing.Size(44, 27);
+            this.txtACoefficient.TabIndex = 0;
+            // 
+            // txtBCoefficient
+            // 
+            this.txtBCoefficient.Location = new System.Drawing.Point(228, 114);
+            this.txtBCoefficient.Name = "txtBCoefficient";
+            this.txtBCoefficient.Size = new System.Drawing.Size(44, 27);
+            this.txtBCoefficient.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(120, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "x²";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(273, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(25, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "x";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(436, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "=   0";
+            // 
+            // txtCCoefficient
+            // 
+            this.txtCCoefficient.Location = new System.Drawing.Point(374, 114);
+            this.txtCCoefficient.Name = "txtCCoefficient";
+            this.txtCCoefficient.Size = new System.Drawing.Size(44, 27);
+            this.txtCCoefficient.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(172, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(204, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "ax² + bx + c = 0";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(140, 170);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 211);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(104, 507);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(94, 46);
+            this.btnCalculate.TabIndex = 11;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(331, 507);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(94, 46);
+            this.btnClear.TabIndex = 12;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // txtFirstRoot
+            // 
+            this.txtFirstRoot.Location = new System.Drawing.Point(88, 436);
+            this.txtFirstRoot.Name = "txtFirstRoot";
+            this.txtFirstRoot.Size = new System.Drawing.Size(125, 27);
+            this.txtFirstRoot.TabIndex = 13;
+            // 
+            // txtSecondRoot
+            // 
+            this.txtSecondRoot.Location = new System.Drawing.Point(313, 436);
+            this.txtSecondRoot.Name = "txtSecondRoot";
+            this.txtSecondRoot.Size = new System.Drawing.Size(125, 27);
+            this.txtSecondRoot.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(134, 396);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 37);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "x₁";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(358, 396);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 37);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "x₂";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(217, 586);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(94, 46);
+            this.btnExit.TabIndex = 17;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(172, 108);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 37);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "+";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(315, 108);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(35, 37);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "+";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 670);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtSecondRoot);
+            this.Controls.Add(this.txtFirstRoot);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCCoefficient);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBCoefficient);
+            this.Controls.Add(this.txtACoefficient);
+            this.Name = "Form1";
+            this.Text = "Quadratic Root Calculator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtACoefficient;
+        private System.Windows.Forms.TextBox txtBCoefficient;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtCCoefficient;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.TextBox txtFirstRoot;
+        private System.Windows.Forms.TextBox txtSecondRoot;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
